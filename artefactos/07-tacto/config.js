eff0070a571672b2057c00525731855e225ce0e9
NOMBRE APELLIDO <amanda.wood@uc.cl>
2026-06-03 22:31:40 -0400

    10:31

--- artefactos/07-tacto/config.js
+++ artefactos/07-tacto/config.js
@@ -1,21 +1,20 @@
-// Tres páginas porque hay tres dispositivos en juego.
 Protobject.setProduction(true);
 
 Protobject.initialize([
   {
     name: "Pantalla",
     page: "index.html",
-    main: true,             // corre en el PC
+    main: true,
     debug: "master",
   },
   {
     name: "Cenital",
-    page: "sensor-cenital.html",   // corre en el teléfono cenital
+    page: "sensor-cenital.html",
     debug: "local",
   },
   {
     name: "Frontal",
-    page: "sensor-frontal.html",   // corre en el teléfono frontal
+    page: "sensor-frontal.html",
     debug: "local",
   }
 ]);
